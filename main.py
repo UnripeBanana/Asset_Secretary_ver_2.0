a6c9dfc01aaebc39e84b1c6033af8c3f9454916a
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-10 21:27:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from config import NOTION_DOMESTIC_STOCK_INFO_DB_ID
 from notion.get_all_pages import get_all_pages
 
+pages = get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID)
+
+for page in pages:
+    print(page["properties"])
+    if page:
+        break
+
+"""
 domestic_stock_main(get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID))
 
     for page in pages:
         ticker = get_ticker(page)
+
+def get_ticker(page):
+    ticker_data = page["properties"]["티커"]["rich_text"]
+
+    if not ticker_data:
+        return None
+
+    return ticker_data[0]["plain_text"]
+"""
