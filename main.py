91b38b6b7243e9b2f58288514b72005d1ab0c959
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-10 22:13:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from config import NOTION_DOMESTIC_STOCK_INFO_DB_ID
 from notion.get_all_pages import get_all_pages
 
-get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID)
+# get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID)
+
+# 네이버는 브라우저가 아닌 프로그램의 요청을 차단하는 경우가 있어서, 브라우저인 척 속이는 역할 수행
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+url = (
+    f"https://polling.finance.naver.com/api/realtime"
+    f"?query=SERVICE_ITEM:{ticker}"
+)
+
+data = requests.get(
+    url,
+    headers=headers,
+    timeout=10 # 최대 10초까지만 기다리겠다는 의미.
+).json()
+
+item = data["result"]["areas"][0]["datas"][0]
+
 
 """
 pages = get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID)
