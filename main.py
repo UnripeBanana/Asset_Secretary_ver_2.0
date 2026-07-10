16750fb0d2d45d28ac8fd32a4650048607f0e234
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-10 21:23:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from config import NOTION_DOMESTIC_STOCK_INFO_DB_ID
 from notion.get_all_pages import get_all_pages
 
 domestic_stock_main(get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID))
+
+    for page in pages:
+        ticker = get_ticker(page)
