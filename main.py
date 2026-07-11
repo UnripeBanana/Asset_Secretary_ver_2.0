b83710d669c6b12d7940032f217b615ed5ef5ba4
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 12:11:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from notion.get_all_pages import get_all_pages
 #-----------------------------------------
 # 국내주식 종목 DB 업데이트
 #-----------------------------------------
-from domestic_stock.read import get_ticker
-from domestic_stock.data import get_domestic_stock_info
-from domestic_stock.update import update_domestic_stock_info_DB
+from domestic_stock_info.read import get_ticker
+from domestic_stock_info.data import get_domestic_stock_info
+from domestic_stock_info.update import update_domestic_stock_info_DB
 
 for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
     # 티커 데이터 추출
