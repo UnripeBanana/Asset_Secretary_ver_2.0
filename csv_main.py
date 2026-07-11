9d91c08c5e8bf4382690481bda78132b563478b4
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 14:55:44 +0900

    Update csv_main.py

--- csv_main.py
+++ csv_main.py
@@ -8,6 +8,13 @@ from domestic_stock_info.read import get_ticker
 from domestic_stock_info.data import get_domestic_stock_info
 from domestic_stock_info.csv.update import append_history
 
+CSV_PATH = Path("domestic_stock_info/csv/price_history.csv")
+
+df = pd.read_csv(
+    CSV_PATH,
+    dtype={"ticker": str}
+)
+
 for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
   # 티커 데이터 추출
   ticker = get_ticker(page)
@@ -16,19 +23,12 @@ for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
 
   # 네이버증권에서 데이터 받아오기
   domestic_stock_info = get_domestic_stock_info(ticker) # dictionary
-
-  CSV_PATH = Path("domestic_stock_info/csv/price_history.csv")
-  
-  df = pd.read_csv(
-      CSV_PATH,
-      dtype={"ticker": str}
-  )
   
   # CSV에 데이터 업로드
   df = append_history(df, domestic_stock_info)
 
-  df.to_csv(
-      CSV_PATH,
-      index=False,
-      encoding="utf-8-sig"
-  )
+df.to_csv(
+    CSV_PATH,
+    index=False,
+    encoding="utf-8-sig"
+)
