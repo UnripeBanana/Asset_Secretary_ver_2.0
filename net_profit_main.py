fd239fbfe7ace33ddf2f8476095ad569f78da860
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 22:41:33 +0900

    Update net_profit_main.py

--- net_profit_main.py
+++ net_profit_main.py
@@ -1,1 +1,14 @@
 from config import NOTION_NET_PROFIT_DB_ID
+from notion.client import notion
+
+def net_profit(prop, profit):
+    notion.pages.create(
+        parent={
+            "database_id": NOTION_NET_PROFIT_DB_ID
+        },
+        properties={
+            "국내주식 수익": {"number": },
+            "국내주식 배당금 수익": {"number": 10},
+            "업데이트 일시": {"number": 72000}
+        }
+    )
