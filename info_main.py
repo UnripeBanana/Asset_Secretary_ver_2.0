4edf047b20b045b35142a8a4c09eb6d66c102b51
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 19:46:47 +0900

    Update info_main.py

--- info_main.py
+++ info_main.py
@@ -26,28 +26,6 @@ for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
     # 네이버증권에서 데이터 받아오기
     domestic_stock_info = get_domestic_stock_info(ticker) # dictionary
 
-    # 노션에 데이터 업로드
+    # 노션 & CSV에 데이터 업로드
     update_domestic_stock_info_DB(page, domestic_stock_info)
 
-
-"""
-pages = get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID)
-
-for page in pages:
-    print(page)
-    if page:
-        break
-##########################
-domestic_stock_main(get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID))
-
-    for page in pages:
-        ticker = get_ticker(page)
-
-def get_ticker(page):
-    ticker_data = page["properties"]["티커"]["rich_text"]
-
-    if not ticker_data:
-        return None
-
-    return ticker_data[0]["plain_text"]
-"""
