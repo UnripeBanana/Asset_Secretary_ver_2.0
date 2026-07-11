ea33bf9b51110380c7cd48708eb0ce69ebcd0ad3
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 14:11:15 +0900

    Update day_log.py

--- utils/day_log.py
+++ utils/day_log.py
@@ -4,5 +4,11 @@ from zoneinfo import ZoneInfo
 def today_is():
   update_time = datetime.now(
     ZoneInfo("Asia/Seoul")
+  ).strftime("%Y-%m-%d")
+  return update_time
+
+def today_and_time_is():
+  update_time = datetime.now(
+    ZoneInfo("Asia/Seoul")
   ).strftime("%Y-%m-%d %H:%M")
   return update_time
