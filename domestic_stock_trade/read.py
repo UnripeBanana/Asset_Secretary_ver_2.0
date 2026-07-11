b00d46cd462ade5abcf92b4e66512822d821f163
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 20:07:09 +0900

    Update read.py

--- domestic_stock_trade/read.py
+++ domestic_stock_trade/read.py
@@ -1,42 +1,29 @@
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
 
-def read_domestic_stock_trade(NOTION_DOMESTIC_STOCK_TRADE_DB_ID):
+def read_domestic_stock_trade(page):
 
-	response = notion.databases.query(
-		database_id = NOTION_DOMESTIC_STOCK_TRADE_DB_ID
-	)
+	props = page["properties"]
 
-	trades = []
+	trade = {
+		"page_id": page["id"],
 
-	for page in response["results"]:
+		# 관계형
+		"ticker": (
+				props["종목명"]["select"]["name"]
+				if props["종목명"]["select"] else None
+		),
+		
+		# 선택
+		"type": props["매수/매도"]["select"]["name"],
 
-		props = page["properties"]
+		# 날짜
+		"date": props["날짜"]["date"]["start"],
 
-		trade = {
-
-			"page_id": page["id"],
-
-			# 관계형
-			"ticker": (
-					props["종목명"]["select"]["name"]
-					if props["종목명"]["select"] else None
-			),
-
-			# 선택
-			"type": props["매수/매도"]["select"]["name"],
-
-			# 날짜
-			"date": props["날짜"]["date"]["start"],
-
-			# 숫자
-			"qty": props["수량"]["number"],
-
-			"price": props["단가"]["number"],
-
-			"amount": props["거래금액"]["formula"]["number"]
-
-		}
-
-		trades.append(trade)
-
-	return trades
+		# 숫자
+		"qty": props["수량"]["number"],
+		"price": props["단가"]["number"],
+		"amount": props["거래금액"]["formula"]["number"]
+	}
+	
+	return trade
+	
