5a8c5a4b0851fe436aad82d8f0549597db63d7ce
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 14:58:03 +0900

    Update csv_main.py

--- csv_main.py
+++ csv_main.py
@@ -7,6 +7,7 @@ from notion.get_all_pages import get_all_pages
 from domestic_stock_info.read import get_ticker
 from domestic_stock_info.data import get_domestic_stock_info
 from domestic_stock_info.csv.update import append_history
+from pathlib import Path
 
 CSV_PATH = Path("domestic_stock_info/csv/price_history.csv")
 
