bf3c866f613b8f2b5ee997f215533b1a0dd0839e
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 12:59:41 +0900

    Update history.py

--- domestic_stock_info/history/history.py
+++ domestic_stock_info/history/history.py
@@ -5,28 +5,6 @@ from zoneinfo import ZoneInfo
 
 CSV_PATH = Path("domestic_stock_info/history/price_history.csv")
 
-def create_csv():
-    if CSV_PATH.exists():
-        return
-
-    columns = [
-        "date",
-        "ticker",
-        "name",
-        "open",
-        "high",
-        "low",
-        "close",
-        "volume",
-        "amount",
-    ]
-
-    pd.DataFrame(columns=columns).to_csv(
-        CSV_PATH,
-        index=False,
-        encoding="utf-8-sig"
-    )
-
 def append_history(domestic_stock_info):
     row = {
         "date": datetime.now(ZoneInfo("Asia/Seoul")).strftime("%Y-%m-%d"),
