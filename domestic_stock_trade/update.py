ab10f78e29b5dc16ea8a6145996eddb8cc0c8232
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 22:46:42 +0900

    Update update.py

--- domestic_stock_trade/update.py
+++ domestic_stock_trade/update.py
@@ -1,4 +1,5 @@
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
+from net_profit import net_profit
 
 def update_domestic_stock_trade_DB(results):
     for id in list(results.keys()):
@@ -7,6 +8,8 @@ def update_domestic_stock_trade_DB(results):
             "잔량": {"number": raw_prop["remaining"]},
             "실현수익": {"number": raw_prop["profit"]}
         }
+
+        net_profit()
 
         notion.pages.update(
             page_id = id,
