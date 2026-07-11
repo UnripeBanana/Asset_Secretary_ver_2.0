38660add1576dc0e2643e54b13e92a7c66d3cb64
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 20:21:48 +0900

    Update trade_main.py

--- trade_main.py
+++ trade_main.py
@@ -37,6 +37,30 @@ for ticker, result in results.items():
 
 
 
+##########################
+for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
+    """
+    # 임시로 노션 페이지 출력 확인
+    print(page)
+    print("\n\n")
+    print(page["properties"])
+    if page:
+        break
+    """
+    
+    # 티커 데이터 추출
+    ticker = get_ticker(page)
+    if not ticker:
+        continue
+
+    # 네이버증권에서 데이터 받아오기
+    domestic_stock_info = get_domestic_stock_info(ticker) # dictionary
+
+    # 노션 & CSV에 데이터 업로드
+    update_domestic_stock_info_DB(page, domestic_stock_info)
+
+
+#######
 
 
 def domestic_stock_trade_main(NOTION_TRADE_DB_ID):
