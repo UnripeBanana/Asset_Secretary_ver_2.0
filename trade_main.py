60cdfabca3a3d508236ff16c0376ed5d4242d596
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 22:04:24 +0900

    Update trade_main.py

--- trade_main.py
+++ trade_main.py
@@ -1,6 +1,5 @@
 from config import NOTION_DOMESTIC_STOCK_TRADE_DB_ID
 from notion.get_all_pages import get_all_pages
-from notion.client import notion
 from collections import defaultdict
 
 #-----------------------------------------
@@ -14,9 +13,9 @@ from domestic_stock_trade.update import update_domestic_stock_trade_DB
 groups = defaultdict(list)
 
 for page in get_all_pages(NOTION_DOMESTIC_STOCK_TRADE_DB_ID):
-    trade = read_domestic_stock_trade(page)  # trade : {'page_id': '9446e5ae-e083-82af-83c7-81578d26b1bf', 'ticker': '삼성전자', 'type': '매수', 'date': '2026-06-19', 'qty': 1, 'price': 349000, 'amount': 349000}
-    groups[trade["ticker"]].append(trade)    # groups : defaultdict(<class 'list'>, {'삼성전자': [{'page_id': '9446e5ae-e083-82af-83c7-81578d26b1bf', 'ticker': '삼성전자', 'type': '매수', 'date': '2026-06-19', 'qty': 1, 'price': 349000, 'amount': 349000}]})
-
+    trade = read_domestic_stock_trade(page)  
+    groups[trade["ticker"]].append(trade)   
+    
 # 읽은 데이터 fifo처리
 for trades in groups.values():
     trades.sort(key=lambda x: x["date"])
