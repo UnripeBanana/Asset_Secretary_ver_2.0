ac4273c99675dd8b867b85f9ea4304e5028dd2e2
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 12:43:34 +0900

    Update history.py

--- domestic_stock_info/history/history.py
+++ domestic_stock_info/history/history.py
@@ -1,1 +1,27 @@
+from pathlib import Path
+import pandas as pd
 
+CSV_PATH = Path("domestic_stock_info/history/price_history.csv")
+
+
+def create_csv():
+    if CSV_PATH.exists():
+        return
+
+    columns = [
+        "date",
+        "ticker",
+        "name",
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+        "amount",
+    ]
+
+    pd.DataFrame(columns=columns).to_csv(
+        CSV_PATH,
+        index=False,
+        encoding="utf-8-sig"
+    )
