9d2fb8aa6b3404b1fc79fdf792c33aaa3a55f81a
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 12:58:39 +0900

    Update history_main.py

--- history_main.py
+++ history_main.py
@@ -1,5 +1,5 @@
 from config import NOTION_DOMESTIC_STOCK_INFO_DB_ID
 from notion.get_all_pages import get_all_pages
-from history.main import domestic_stock_history_main
+from domestic_stock_info.history.main import domestic_stock_info_history_main
 
 domestic_stock_info_history_main(get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID))
