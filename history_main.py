63579a42e8a0625326007c7d774125e57491cca2
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-11 12:58:10 +0900

    Update history_main.py

--- history_main.py
+++ history_main.py
@@ -2,10 +2,4 @@ from config import NOTION_DOMESTIC_STOCK_INFO_DB_ID
 from notion.get_all_pages import get_all_pages
 from history.main import domestic_stock_history_main
 
-
-def main():
-    pages = get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID)
-    domestic_stock_history_main(pages)
-
-if __name__ == "__main__":
-    main()
+domestic_stock_info_history_main(get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID))
