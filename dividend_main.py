4dcb95401131f092154069fa20406729cae4d457
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-12 10:19:21 +0900

    Update dividend_main.py

--- dividend_main.py
+++ dividend_main.py
@@ -5,19 +5,11 @@ from notion.get_all_pages import get_all_pages
 # 국내주식 거래내역 DB 업데이트
 #-----------------------------------------
 from domestic_stock_trade.read import read_domestic_stock_trade
-from trade.fifo import process_fifo
 from domestic_stock_trade.update import update_domestic_stock_trade_DB
 
-# 각 페이지별로 데이터 읽기
 for page in get_all_pages(NOTION_DOMESTIC_STOCK_TRADE_DB_ID):
-    trade = read_domestic_stock_trade(page)  
-    groups[trade["ticker"]].append(trade)   
+    # 각 페이지별로 데이터 읽기
+    properties = read_domestic_stock_dividend(page)
     
-# 읽은 데이터 fifo처리
-for trades in groups.values():
-    trades.sort(key=lambda x: x["date"])
-
-results = process_fifo(groups)
-
-# 노션에 데이터 업데이트
-update_domestic_stock_trade_DB(results)
+    # 노션에 데이터 업데이트
+    update_domestic_stock_trade_DB(properties)
