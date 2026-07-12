b64d6b16ae1970111c33e5f1ad59ddea204c1fd3
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-12 10:23:07 +0900

    Update dividend_main.py

--- dividend_main.py
+++ dividend_main.py
@@ -7,7 +7,7 @@ from notion.get_all_pages import get_all_pages
 from dividend.read import read_dividend
 from net_profit import net_profit
 
-for page in get_all_pages(NOTION_DOMESTIC_STOCK_TRADE_DB_ID):
+for page in get_all_pages(NOTION_DOMESTIC_STOCK_DIVIDEND_DB_ID):
     # 각 페이지별로 데이터 읽기
     profit = read_dividend(page)
     
