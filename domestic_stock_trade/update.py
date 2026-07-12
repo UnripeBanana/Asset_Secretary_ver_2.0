d85defff698fa2dfb086ae0975b9f4ac181cf1cf
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-12 09:41:26 +0900

    Update update.py

--- domestic_stock_trade/update.py
+++ domestic_stock_trade/update.py
@@ -1,14 +1,16 @@
 from notion.client import notion # notion : 로그인 된 앱에 접근할 수 있도록 해주는 역할
 from net_profit import net_profit
 
-def update_domestic_stock_trade_DB(results, trades):
+# 'e816e5ae-e083-836a-9f48-01e15f18cd77': {'ticker': '한온시스템', 'date': '2026-06-18', 'profit_saved': False, 'remaining': 1, 'profit': 0}
+
+def update_domestic_stock_trade_DB(results):
     for id, raw_prop in results.items():
         properties = {
             "잔량": {"number": raw_prop["remaining"]},
             "실현수익": {"number": raw_prop["profit"]}
         }
 
-        if raw_prop["profit"] and not trade["profit_saved"]: 
+        if raw_prop["profit"] and not raw_prop["profit_saved"]: 
             net_profit("domestic_stock", raw_prop["profit"])
             properties["순수익 기록"] = {
                 "checkbox": True
