8f64dc2487544adb7e01b7c949a4aaa385661254
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-12 12:11:37 +0900

    Update data.py

--- krx_gold/data.py
+++ krx_gold/data.py
@@ -1,5 +1,18 @@
+from naver_finance import put_ticker_to_get_naver_prop
+
 def get_krx_gold_info():
+    properties = put_ticker_to_get_naver_prop(ticker)  # dictionary
+
+    return properties
+
+# naver_finance.py에서 함수를 호출해서 증권사 데이터 뽑아오기
+
+from naver_finance import put_ticker_to_get_naver_prop
+
+def get_domestic_stock_info(ticker):
+  properties = put_ticker_to_get_naver_prop(ticker)  # dictionary
   
+  return properties
 
 
 
