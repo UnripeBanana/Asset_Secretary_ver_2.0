5806c6dc1a3c354b7beb99ebc7a37b6a593b020c
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-12 10:20:30 +0900

    Update dividend_main.py

--- dividend_main.py
+++ dividend_main.py
@@ -5,11 +5,11 @@ from notion.get_all_pages import get_all_pages
 # 국내주식 거래내역 DB 업데이트
 #-----------------------------------------
 from domestic_stock_trade.read import read_domestic_stock_trade
-from domestic_stock_trade.update import update_domestic_stock_trade_DB
+from net_profit import net_profit
 
 for page in get_all_pages(NOTION_DOMESTIC_STOCK_TRADE_DB_ID):
     # 각 페이지별로 데이터 읽기
     properties = read_domestic_stock_dividend(page)
     
     # 노션에 데이터 업데이트
-    update_domestic_stock_trade_DB(properties)
+    net_profit("", properties)
