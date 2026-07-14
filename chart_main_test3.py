1faffe45234a2309d2258297cabf666ce328066b
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-14 20:56:06 +0900

    Update chart_main_test3.py

--- chart_main_test3.py
+++ chart_main_test3.py
@@ -102,7 +102,7 @@ ax.set_ylim(
 # -----------------------------
 ax.plot(
     high_idx,
-    high_price + margin * 0.08,
+    high_price + margin * 0.15,
     marker="v",                          # ▼ 표시
     color="gray",
     markersize=5
@@ -111,7 +111,7 @@ ax.plot(
 # 텍스트
 ax.text(
     high_idx + 0.5,                      # 왼쪽으로 약간 이동
-    high_price + margin * 0.08,          # ▼와 같은 높이
+    high_price + margin * 0.15,          # ▼와 같은 높이
     f"High Price {high_price:,}",
     va="center",
     ha="right",
@@ -124,7 +124,7 @@ ax.text(
 # -----------------------------
 ax.plot(
     low_idx,
-    low_price - margin * 0.08,
+    low_price - margin * 0.15,
     marker="^",                          # ▲ 표시
     color="gray",
     markersize=5
@@ -133,7 +133,7 @@ ax.plot(
 # 텍스트
 ax.text(
     low_idx + 0.5,                       # 오른쪽으로 약간 이동
-    low_price - margin * 0.08,           # ▲와 같은 높이
+    low_price - margin * 0.15,           # ▲와 같은 높이
     f"Low Price {low_price:,}",
     va="center",
     ha="left",
