8bf201083cbdb55921afad4954d145488f188fc5
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-14 16:05:56 +0900

    Update chart_main.py

--- chart_main.py
+++ chart_main.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("domestic_stock_info/csv/price_history.csv")
 
-print(df.head())
+# print(df.head())
+
+ticker = "005930"
+
+stock = df[df["ticker"] == ticker].copy()
+
+stock["date"] = pd.to_datetime(stock["date"])
+
+stock = stock.set_index("date")
