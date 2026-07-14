5ab56e8c044b270c91f8e9cf1826c4c6fa2f266b
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-14 14:44:44 +0900

    Update csv_main.py

--- csv_main.py
+++ csv_main.py
@@ -1,4 +1,4 @@
-from config import NOTION_DOMESTIC_STOCK_INFO_DB_ID, NOTION_KRX_GOLD_INFO_DB_ID
+from config import NOTION_DOMESTIC_STOCK_INFO_DB_ID, NOTION_KRX_GOLD_INFO_DB_ID, NOTION_DOMESTIC_BOND_ETF_INFO_DB_ID
 from notion.get_all_pages import get_all_pages
 from pathlib import Path
 import pandas as pd
