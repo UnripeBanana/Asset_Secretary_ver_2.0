796443b30cbe8f3cc8c23d65a17c6d3cf6586307
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-14 16:27:32 +0900

    Update chart_main.py

--- chart_main.py
+++ chart_main.py
@@ -15,10 +15,26 @@ stock = stock.set_index("date")
 
 print(stock.head())
 
+mc = mpf.make_marketcolors(
+    up='#e53935',      # 상승(빨강)
+    down='#1565c0',    # 하락(파랑)
+    edge='inherit',
+    wick='inherit',
+    volume='inherit'
+)
+
+style = mpf.make_mpf_style(
+    marketcolors=mc,
+    gridstyle='--',
+    facecolor='white',
+    figcolor='white'
+)
+
 mpf.plot(stock, type="candle")
 
 mpf.plot(
     stock,
     type="candle",
+    style = style,
     savefig="charts/005930.png"
 )
