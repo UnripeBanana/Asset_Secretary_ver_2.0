9db57a34db6b8327f9f230fc91fc1c91417a4156
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-14 20:39:32 +0900

    Update chart_main_test3.py

--- chart_main_test3.py
+++ chart_main_test3.py
@@ -110,7 +110,7 @@ ax.plot(
 
 # 텍스트
 ax.text(
-    high_idx - 5,      # 왼쪽으로 약간 이동
+    high_idx - 2.5,      # 왼쪽으로 약간 이동
     high_price,          # ▼와 같은 높이
     f"High Price {high_price:,}",
     va="center",
