62c3799002714ac335b19397648d55b53b834a3c
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-14 14:41:41 +0900

    Update csv_main.py

--- csv_main.py
+++ csv_main.py
@@ -60,3 +60,35 @@ krx_gold_df.to_csv(
     index=False,
     encoding="utf-8-sig"
 )
+
+#-----------------------------------------
+# 국내 채권 ETF CSV 업데이트
+#-----------------------------------------
+from domestic_stock_info.read import get_ticker
+from domestic_stock_info.data import get_domestic_stock_info
+from domestic_stock_info.csv.update import append_domestic_stock_history
+
+DOMESTIC_STOCK_CSV_PATH = Path("domestic_bond_etf_info/csv/price_history.csv")
+
+domestic_stock_df = pd.read_csv(
+    DOMESTIC_STOCK_CSV_PATH,
+    dtype={"ticker": str}
+)
+
+for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
+  # 티커 데이터 추출
+  ticker = get_ticker(page)
+  if not ticker:
+      continue
+
+  # 네이버증권에서 데이터 받아오기
+  domestic_stock_info = get_domestic_stock_info(ticker) # dictionary
+  
+  # CSV에 데이터 업로드
+  domestic_stock_df = append_domestic_stock_history(domestic_stock_df, domestic_stock_info)
+
+domestic_stock_df.to_csv(
+    DOMESTIC_STOCK_CSV_PATH,
+    index=False,
+    encoding="utf-8-sig"
+)
