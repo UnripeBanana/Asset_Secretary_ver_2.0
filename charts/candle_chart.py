f7e83fff46721dbbec489ff4afd886de4c432755
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-19 12:59:11 +0900

    Update candle_chart.py

--- charts/candle_chart.py
+++ charts/candle_chart.py
@@ -1,17 +1,13 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-
 # stock
 
-date  ticker  name    open  ...     low   close    volume        amount
-0 2026-07-13  005930  삼성전자  285000  ...  253000  254500  31882652  8.455131e+12
-1 2026-07-14  005930  삼성전자  255000  ...  247000  263000  39989493  1.039033e+13
+#date  ticker  name    open  ...     low   close    volume        amount
+#0 2026-07-13  005930  삼성전자  285000  ...  253000  254500  31882652  8.455131e+12
+#1 2026-07-14  005930  삼성전자  255000  ...  247000  263000  39989493  1.039033e+13
+#[2 rows x 9 columns]
 
-[2 rows x 9 columns]
+from matplotlib.patches import Rectangle
 
-def make_candle_chart(stock):
+def make_candle_chart(ax, stock):
     for i, row in stock.iterrows():
         open_price = row["open"]
         high_price = row["high"]
@@ -19,7 +15,7 @@ def make_candle_chart(stock):
         close_price = row["close"]
     
         # 상승 / 하락 색상
-        color = "#e53935" if close_price >= open_price else "#1565c0"
+        color = "#e53935" if close_price > open_price else "#1565c0" if close_price < open_price else "#000000"
     
         # 심지
         ax.vlines(
