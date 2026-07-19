a2d23487bdca79efaef82b6eb2e88d7589a2d140
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-19 12:43:00 +0900

    Update read_csv.py

--- charts/read_csv.py
+++ charts/read_csv.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 def read_csv(path, ticker):
-  df = pd.read_csv(path, dtype={"ticker": str})
-  
-  stock = df[df["ticker"] == ticker].copy()
-  
-  stock["date"] = pd.to_datetime(stock["date"])
-  stock = stock.sort_values("date").reset_index(drop=True)
+    df = pd.read_csv(path, dtype={"ticker": str})
+    
+    stock = df[df["ticker"] == ticker].copy()
+    
+    stock["date"] = pd.to_datetime(stock["date"])
+    stock = stock.sort_values("date").reset_index(drop=True)
 
-  return stock  
+    print(stock)
+    
+    return stock  
